3c09281cc736edd5b981a085fd3054e34aff95b9
FukcThat <63652e636f6465@gmail.com>
2024-08-12 23:36:41 +0200

    INIT: Set up html form Added basic css Got all variables set up

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(5);
+const form = document.querySelector("form");
+const emailInput = form.querySelector("#input__email");
+const emailInputError = form.querySelector("#input__email--error");
+const countryInput = form.querySelector("#input__country");
+const countryInputError = form.querySelector("#input__country--error");
+const zipcodeInput = form.querySelector("#input__zipcode");
+const zipcodeInputError = form.querySelector("#input__zipcode--error");
+const passwordInput = form.querySelector("#input__password");
+const passwordInputError = form.querySelector("#input__password--error");
+const passwordConfirmInput = form.querySelector("#input__password-confirm");
+const passwordConfirmInputError = form.querySelector(
+  "#input__password-confirm--error"
+);
+const submitBtn = form.querySelector("#submit-btn");
