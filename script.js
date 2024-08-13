0a26979930a2fb69dc3eebc8fddaafb0d2442f00
FukcThat <63652e636f6465@gmail.com>
2024-08-13 18:31:41 +0200

    FEAT: Added validation to Email Input field Added Country Select

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 const form = document.querySelector("form");
-const emailInput = form.querySelector("#input__email");
-const emailInputError = form.querySelector("#input__email--error");
-const countryInput = form.querySelector("#input__country");
-const countryInputError = form.querySelector("#input__country--error");
-const zipcodeInput = form.querySelector("#input__zipcode");
-const zipcodeInputError = form.querySelector("#input__zipcode--error");
-const passwordInput = form.querySelector("#input__password");
-const passwordInputError = form.querySelector("#input__password--error");
-const passwordConfirmInput = form.querySelector("#input__password-confirm");
-const passwordConfirmInputError = form.querySelector(
+const emailInput = document.querySelector("#input__email");
+const emailInputError = document.querySelector("#input__email--error");
+const countryInput = document.querySelector("#input__country");
+const countryInputError = document.querySelector("#input__country--error");
+const zipcodeInput = document.querySelector("#input__zipcode");
+const zipcodeInputError = document.querySelector("#input__zipcode--error");
+const passwordInput = document.querySelector("#input__password");
+const passwordInputError = document.querySelector("#input__password--error");
+const passwordConfirmInput = document.querySelector("#input__password-confirm");
+const passwordConfirmInputError = document.querySelector(
   "#input__password-confirm--error"
 );
-const submitBtn = form.querySelector("#submit-btn");
+const submitBtn = document.querySelector("#submit-btn");
+
+// Event Listeners
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!emailInput.validity.valid) {
+    showError();
+  }
+});
+
+emailInput.addEventListener("input", () => {
+  if (emailInput.validity.valid) {
+    emailInputError.textContent = "";
+    emailInputError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+// Functions
+
+const showError = () => {
+  if (emailInput.validity.valueMissing) {
+    emailInputError.textContent = "Email pls.";
+  } else if (emailInput.validity.typeMismatch) {
+    emailInputError.textContent = "This is not Email. Make it Email pls.";
+  }
+
+  emailInputError.className = "error active";
+};
