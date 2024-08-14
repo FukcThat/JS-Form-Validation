d43950d80a998c57d4e7334cc1b675318d27a308
FukcThat <63652e636f6465@gmail.com>
2024-08-14 22:10:09 +0200

    FEAT: Added validation to zip-code input

--- script.js
+++ script.js
@@ -15,25 +15,44 @@ const submitBtn = document.querySelector("#submit-btn");
 
 // Event Listeners
 
+// Form Submit EventListener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (!emailInput.validity.valid) {
-    showError();
+  if (!emailInput.validity.valid || !zipcodeInput.validity.valid) {
+    // or country, zipcode, password, confirm password are NOT valid
+    showEmailError();
+    showZipcodeError();
   }
 });
 
+// E-Mail Input EventListener
 emailInput.addEventListener("input", () => {
   if (emailInput.validity.valid) {
     emailInputError.textContent = "";
     emailInputError.className = "error";
   } else {
-    showError();
+    showEmailError();
   }
 });
 
+// Country input eventListener
+
+// ZIP-Code EventListener
+zipcodeInput.addEventListener("input", () => {
+  if (zipcodeInput.validity.valid) {
+    zipcodeInputError.textContent = "";
+    zipcodeInputError.className = "error";
+  } else {
+    showZipcodeError();
+  }
+});
+
+// Password EventListener
+// Confirm Password Eventlistener
+
 // Functions
 
-const showError = () => {
+const showEmailError = () => {
   if (emailInput.validity.valueMissing) {
     emailInputError.textContent = "Email pls.";
   } else if (emailInput.validity.typeMismatch) {
@@ -41,4 +60,15 @@ const showError = () => {
   }
 
   emailInputError.className = "error active";
+};
+
+// Zipcode Error
+
+const showZipcodeError = () => {
+  if (zipcodeInput.validity.valueMissing) {
+    zipcodeInputError.textContent = "Zip Code too, pls.";
+  } else if (zipcodeInput.validity.typeMismatch) {
+    zipcodeInputError.textContent = "Numbers only, ok?";
+  }
+  zipcodeInputError.className = "error active";
 };
